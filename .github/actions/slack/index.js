6154e50488ae597732b6633bf59f854c0574ef88
Nick Novitski <github@nicknovitski.com>
2020-06-08 16:50:54 -0700

    test

---
+++ .github/actions/slack/index.js
@@ -0,0 +1,48 @@
+const querystring = require('querystring');
+const http = require('http');
+const { URL } = require('url');
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const text = core.getInput('text');
+  core.info(message)
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  core.info(`The event payload: ${payload}`);
+
+  const data = querystring.stringify({ text });
+
+  const webhookUrl = process.env.SLACK_WEBHOOK_URL;
+
+  if (webhookUrl === undefined) {
+    throw new Error('Specify secrets.SLACK_WEBHOOK_URL');
+  }
+
+  core.setSecret(webhookUrl);
+
+  const url = new URL(webhookUrl);
+
+  const options = {
+      host: url.host,
+      path: url.path,
+      headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(post_data)
+      }
+  };
+
+  const request = http.request(options, function(res) {
+    core.info('response status:', res.statusCode);
+    core.info('response headers:', res.headers);
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        core.info('Response: ' + chunk);
+    });
+  });
+
+  request.write(data);
+  request.end();
+} catch (error) {
+  core.setFailed(error.message);
+}
